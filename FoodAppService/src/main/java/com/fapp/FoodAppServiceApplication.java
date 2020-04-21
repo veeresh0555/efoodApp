cd3477aa25837b2a7f8777c88936efe55f989fad
veeru <veeru@DESKTOP-EPVTAH2>
2020-04-21 16:56:33 +0530

    new

--- FoodAppService/src/main/java/com/fapp/FoodAppServiceApplication.java
+++ FoodAppService/src/main/java/com/fapp/FoodAppServiceApplication.java
@@ -3,11 +3,15 @@ package com.fapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import com.fapp.config.RibbonConfiguration;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@RibbonClient(name="foodappsevice",configuration = RibbonConfiguration.class)
 public class FoodAppServiceApplication {
 
 	public static void main(String[] args) {
